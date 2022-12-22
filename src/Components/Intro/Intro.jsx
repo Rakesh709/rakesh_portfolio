428a42e89f9456ad5d6019b3f7c841c027c8a3c8
Rakesh709 <kumar.rakesh1997@outlook.com>
2022-12-22 13:24:36 +0530

    Added Intro Component

---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Intro.css';
+
+function Intro() {
+  return (
+    <div className="intro">
+        <div className="i-left">
+            <div className="i-name">
+                <span>hy! I Am</span>
+                <span>Rakesh Kumar</span>
+                <span>Front end Developer with high level of experience i web designing and development, producting the quality work</span>
+            </div>
+            <button className="button i-button">Hire Me</button>
+        </div>
+        <div className="i-right">
+            i am right side
+        </div>
+    </div>
+  )
+}
+
+export default Intro
