2ab0a73a0325153ef19375daf8f38a8e1dfc490f
Rakesh709 <kumar.rakesh1997@outlook.com>
2022-12-22 21:20:20 +0530

    Modificaton in Intro by FloatingDiv

---
+++ src/Components/FloatingDiv/FloatingDiv.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './FloatingDiv.css';
+function FloatingDiv(props) {
+  return (
+    <div className="floatingdiv">
+        <img src={props.image} alt="" />
+        <span>
+           {props.txt1}
+            <br />
+            {props.txt2}
+        </span>
+    </div>
+  )
+}
+
+export default FloatingDiv
--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -3,6 +3,13 @@ import './Intro.css';
 import Github from '../../img/github.png';
 import LinkedIn from '../../img/linkedin.png';
 import Instagram from '../../img/instagram.png';
+import Vector1 from '../../img/Vector1.png';
+import Vector2 from '../../img/Vector2.png';
+import boy from '../../img/boy.png';
+import thumbup from '../../img/thumbup.png';
+import Crown from '../../img/crown.png';
+import glassesimoji from '../../img/glassesimoji.png';
+import FloatingDiv from '../FloatingDiv/FloatingDiv';
 
 
 
@@ -23,7 +30,20 @@ function Intro() {
             </div>
         </div>
         <div className="i-right">
-            i am right side
+            <img src={Vector1} alt="" />
+            <img src={Vector2} alt="" />
+            <img src={boy} alt="" />
+            <img src={glassesimoji} alt="" />
+            <div style={{top:'-4%',left:'68%'}}>
+              <FloatingDiv image={Crown} txt1='Web' txt2='Developer'/>
+            </div>
+            <div style={{top:'18rem',left:'0rem'}}>
+              <FloatingDiv image={thumbup} txt1='Best Design' txt2='Award' />
+            </div>
+            {/* blur divs */}
+            <div className="blur" style={{background:'rgb(238,210,255)'}}>
+
+            </div>
         </div>
     </div>
   )
