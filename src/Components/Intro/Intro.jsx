29be2d9c86e4924f777ded1dc1057cc30413b46b
Rakesh709 <kumar.rakesh1997@outlook.com>
2022-12-22 15:45:47 +0530

    Modification in Intro Component

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import './Intro.css';
+import Github from '../../img/github.png';
+import LinkedIn from '../../img/linkedin.png';
+import Instagram from '../../img/instagram.png';
+
+
 
 function Intro() {
   return (
@@ -11,6 +16,11 @@ function Intro() {
                 <span>Front end Developer with high level of experience i web designing and development, producting the quality work</span>
             </div>
             <button className="button i-button">Hire Me</button>
+            <div className="i-icons">
+              <a href="https://github.com/Rakesh709"><img src={Github} alt="github" /></a>
+              <a href="https://www.linkedin.com/in/rakesh-kumar-6b3228145/"><img src={LinkedIn} alt="linkedin" /></a>
+              <a href="https://www.instagram.com/"> <img src={Instagram} alt="instagram" /></a>
+            </div>
         </div>
         <div className="i-right">
             i am right side
