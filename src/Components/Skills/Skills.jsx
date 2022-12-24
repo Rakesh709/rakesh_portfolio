7dfa6780c2d19beb1d0a6a820e1ad297e84c59cc
Rakesh709 <kumar.rakesh1997@outlook.com>
2022-12-24 20:10:28 +0530

    Modification in Skills component

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -12,10 +12,13 @@ function Skills() {
             <div className="awesome">
                 <span>My</span>
                 <span>Skills</span>
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/> Exercitationem vero, sequi autem, similique sapiente no nisi.</span>
+                <button className="button s-button">Download CV</button>
+                <div className="blur s-blur1" style={{background:'#BF1FF94'}}></div>
             </div>
             {/* right side */}
             <div className="cards">
-
+                I AM RIGHT SIDE
             </div>
         </div>
   )
