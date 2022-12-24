c71cb57a6d332d68e2f19c9ef10984ba75284e3d
Rakesh709 <kumar.rakesh1997@outlook.com>
2022-12-24 19:57:30 +0530

    Added Skills component

---
+++ src/Components/Skills/Skills.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Skills.css';
+
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
+
+function Skills() {
+  return (
+        <div className="skills">
+            {/* life side */}
+            <div className="awesome">
+                <span>My</span>
+                <span>Skills</span>
+            </div>
+            {/* right side */}
+            <div className="cards">
+
+            </div>
+        </div>
+  )
+}
+
+export default Skills
