26b939b389311b28af4d36b9474e8349fef2d969
Rakesh709 <kumar.rakesh1997@outlook.com>
2022-12-28 23:45:22 +0530

    Modification in work component

--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -19,7 +19,28 @@ function Works() {
                 <button className="button s-button">Hire Me</button>
                 <div className="blur s-blur1" style={{background:'#C1F5FF'}}></div>
             </div>
-
+            {/* right side */}
+            <div className="w-right">
+              <div className="w-mainCircle">
+                <div className="w-secCircle">
+                  <img src={Upwork} alt="upwork" />
+                </div>
+                <div className="w-secCircle">
+                  <img src={Fiverr} alt="fiver" />
+                </div>
+                <div className="w-secCircle">
+                  <img src={Amazon} alt="Amazon" />
+                </div>
+                <div className="w-secCircle">
+                  <img src={Shopify} alt="Shopify" />
+                </div>
+                <div className="w-secCircle">
+                  <img src={Facebook} alt="Facebook" />
+                </div>
+              </div>
+              <div className="w-backCircle blueCircle"></div>
+              <div className="w-backCircle yellowCircle"></div>
+            </div>
     </div>
    
   )
