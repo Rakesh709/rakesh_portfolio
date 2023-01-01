8af165bacb46286a04eb1bd5b4a442849931acfd
Rakesh709 <kumar.rakesh1997@outlook.com>
2023-01-01 19:29:05 +0530

    Added animation in skills components

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -8,7 +8,13 @@ import Card from '../Card/Card.jsx';
 
 import Resume from "./resume.pdf";
 
+import {motion} from "framer-motion";
+
 function Skills() {
+    //for transition
+    const transition = {duration: 1 , type:"spring"}
+
+
   return (
         <div className="skills" id="Skills">
             {/* life side */}
@@ -23,27 +29,43 @@ function Skills() {
             </div>
             {/* right side */}
             <div className="cards">
-                <div style={{left:"25rem"}}>
+                <motion.div 
+                    whileInView={{left:"18rem"}}
+                    initial={{left:"30rem"}}
+                    transition={transition}
+                
+                style={{left:"25rem"}}>
                         <Card
                         emoji={HeartEmoji}
                         heading={"Programming"}
                         detail={"JavaScript, Java, MySql"}
                         />
-                </div>
-                <div style={{top:"12rem",left:"3rem"}}>
+                </motion.div>
+                <motion.div 
+                initial={{left:"-11rem",top:"12rem"}}
+                whileInView={{left:"-2rem"}}
+                transition={transition}
+
+
+                style={{top:"12rem",left:"3rem"}}>
                         <Card
                         emoji={Glasses}
                         heading={"Developer"}
                         detail={"Html , Css , Bootstrap, React"}
                         />
-                </div>
-                <div style={{top:"19rem",left:"19rem"}}>
+                </motion.div>
+                <motion.div 
+                initial={{ top: "19rem", left: "15rem" }}
+                whileInView={{ left: "20rem" }}
+                transition={transition}
+                
+                style={{top:"19rem",left:"19rem"}}>
                         <Card
                         emoji={Humble}
                         heading={"Linux"}
                         detail={"Kali Linux , Ubuntu"}
                         />
-                </div>
+                </motion.div>
                 <div className="blur s-blur2" style={{background:"var(--purple"}}></div>
             </div>
         </div>
