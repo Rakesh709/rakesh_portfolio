8294836353bd83e51a9f8a257b078f7007c0b561
Rakesh709 <kumar.rakesh1997@outlook.com>
2023-01-03 12:32:43 +0530

    Adding darkMode in Toggle

--- src/App.js
+++ src/App.js
@@ -8,9 +8,22 @@ import Portfolio from "./Components/Portfolio/Portfolio";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
 
+import {themeContext} from "./Context.js";
+import { useContext } from "react";
+
 function App() {
+  const theme=useContext(themeContext);
+  const darkMode=theme.state.darkMode;
+
   return (
-    <div className="App">
+    <div className="App"
+    style={{
+      background: darkMode ? 'black' :'',
+      color: darkMode ? "white": ''
+    }}
+    
+    
+    >
       <Navbar/>
       <Intro/>
       <Skills/>
--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { ThemeProvider } from './Context';
+
 
 ReactDOM.render(
-    <App />,
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
