2b4493288d348bd64b395c0fd4eb51a0b4308c5d
Rakesh709 <kumar.rakesh1997@outlook.com>
2023-01-04 23:59:55 +0530

    changes in  toggle

--- src/Components/Toggle/Toggle.jsx
+++ src/Components/Toggle/Toggle.jsx
@@ -3,13 +3,23 @@ import "./Toggle.css";
 
 import Sun from "@iconscout/react-unicons/icons/uil-sun";
 import Moon from "@iconscout/react-unicons/icons/uil-moon";
+import { themeContext } from '../../Context';
+import { useContext } from 'react';
 
 function Toggle() {
+
+  const theme =useContext(themeContext);
+  const darkMode =theme.darkMode;
+
+
+
   return (
     <div className="toggle">
-        <Sun/>
         <Moon/>
-        <div className="t-button">
+        <Sun/>
+        <div className="t-button"
+        style={darkMode? {left:'2px'}: {right:'2px'}}
+        >
           
         </div>
     </div>
