bcd36bc1a8c24e7bf76ab6e071178bf5fee0a385
Rakesh709 <kumar.rakesh1997@outlook.com>
2023-01-06 22:50:25 +0530

    Modification in Toggle Click

--- src/Components/Toggle/Toggle.jsx
+++ src/Components/Toggle/Toggle.jsx
@@ -12,12 +12,15 @@ function Toggle() {
   const darkMode =theme.darkMode;
 
 
-
+  const handleClick=()=>{
+    theme.dispatch({type:'toggle'})
+  }
   return (
-    <div className="toggle">
+    <div className="toggle" onClick={handleClick}>
         <Moon/>
         <Sun/>
         <div className="t-button"
+        
         style={darkMode? {left:'2px'}: {right:'2px'}}
         >
           
